ed54937b8cd31173e76fb95a7e78e32d8cbc6f39
izvest <eolsti@gmail.com>
2019-02-25 20:51:58 +0200

    ex 1.6-1.7 done

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -63,3 +63,61 @@ const Total = (props) => {
   }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
+class Feed extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pos: 0,
+      neg: 0,
+      neut: 0,
+      total: 0
+    }
+  }
+
+  klikPos = () => {
+    this.setState({
+      pos: this.state.pos + 1,
+      total: this.state.total + 1
+    })
+  }
+
+  klikNeg = () => {
+    this.setState({
+      neg: this.state.neg + 1,
+      total: this.state.total + 1
+    })
+  }
+
+  klikNeut = () => {
+    this.setState({
+      neut: this.state.neut + 1,
+      total: this.state.total + 1
+    })
+  }
+
+  render() {
+    
+    return (
+      <div>
+        <div>
+          <h1>Anna palautetta</h1>
+          <button onClick={this.klikPos}>Posiiivinen</button>
+          <button onClick={this.klikNeut}>Neutraali</button>
+          <button onClick={this.klikNeg}>Negatiivinen</button>
+          <br></br>
+          <p>Positiivisia: {this.state.pos}</p>
+          <p>Neutraaleja: {this.state.neut}</p>
+          <p>Negatiivisia: {this.state.neg}</p>
+          <br></br>
+          <p>Total times feedback given: {this.state.total}</p>
+          <p>Keskiarvo: {Math.round(100*(this.state.pos-this.state.neg)/this.state.total)/100}</p>
+          <p>Positiivisia: {Math.round(100*this.state.pos/this.state.total)} %</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Feed />, document.getElementById('feedback'));
