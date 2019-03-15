5d64958dfb61d512f9ea1651f61549e2699b993a
izvest <eolsti@gmail.com>
2019-03-15 12:23:53 +0200

    ex 2.7 done

--- osa2/src/Puhelinluettelo.js
+++ osa2/src/Puhelinluettelo.js
@@ -12,31 +12,14 @@ class Puhelinluettelo extends React.Component {
     }
   }
 
-  render() {
-    return (
-      <div>
-        <h2>Puhelinluettelo</h2>
-        <form onSubmit={this.addName}>
-          <div>
-            nimi: <input 
-              value={this.state.newName}
-              onChange={this.handleNameChange}/>
-          </div>
-          <div>
-            numero: <input 
-              value={this.state.newNumber}
-              onChange={this.handleNumberChange}/>
-          </div>
-          <div>
-            <button type="submit">lisää</button>
-          </div>
-        </form>
-        <h2>Numerot</h2>
-        <ul>
-          {this.state.persons.map(n => <li>{n.name}  {n.number}</li>)}
-        </ul>
-      </div>
-    )
+  handleNameChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newName: event.target.value })
+  }
+
+  handleNumberChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ newNumber: event.target.value })
   }
 
   addName = (event) => {
@@ -46,10 +29,8 @@ class Puhelinluettelo extends React.Component {
       number: this.state.newNumber
     }
 
-    if (nameObject.name === '' || nameObject.number === ''){
-      alert("every contact needs both name and number")
-      
-    }else{
+    if (nameObject.name === '' || nameObject.number === ''){alert("every contact needs both name and number")}
+    else{
       if (this.state.persons.map(e => e.name).indexOf(nameObject.name) !== -1){
         alert("person already on the list")
         this.setState({
@@ -68,14 +49,52 @@ class Puhelinluettelo extends React.Component {
     }
   }
 
-  handleNameChange = (event) => {
-    console.log(event.target.value)
-    this.setState({ newName: event.target.value })
-  }
-  handleNumberChange = (event) => {
-    console.log(event.target.value)
-    this.setState({ newNumber: event.target.value })
+  render() {
+    return (
+      <div>
+        <h2>Puhelinluettelo</h2>
+        <Form caller={this}/>
+        <List caller={this}/>
+      </div>
+    )
   }
 }
+
+const List = ({caller}) => (
+  <div>
+    <h2>Numerot</h2>
+    <ul>
+      {caller.state.persons.map(n => <li>{n.name}  {n.number}</li>)}
+    </ul>
+  </div>
+)
+
+const Form = ({caller,submit}) => (
+  <form onSubmit={caller.addName}>
+      <Inputs caller={caller}/>
+      <Button type="submit"/>
+    </form>
+)
+
+const Inputs = ({caller}) => (
+  <div>
+    <div>
+      nimi: <input 
+        value={caller.state.newName}
+        onChange={caller.handleNameChange}/>
+    </div>
+    <div>
+      numero: <input 
+        value={caller.state.newNumber}
+        onChange={caller.handleNumberChange}/>
+    </div>
+  </div>
+)
+
+const Button = (type) => (
+  <div>
+    <button type={type}>lisää</button>
+  </div>
+) 
 
 export default Puhelinluettelo
