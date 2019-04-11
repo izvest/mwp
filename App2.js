ab3bafb27827018cb7f18b2240188b7a34e317a0
Emil Holsti <izvest@users.noreply.github.com>
2019-04-11 13:55:10 +0300

    Update App2.js

--- App2.js
+++ App2.js
@@ -11,7 +11,7 @@ const AppNavigator = createStackNavigator(
     Notes: NotesView,
     Add: AddView
   },
-  {initialRouteName: "Notes"}
+  {initialRouteName: "Bank"}
 );
 
 const AppContainer = createAppContainer(AppNavigator);
